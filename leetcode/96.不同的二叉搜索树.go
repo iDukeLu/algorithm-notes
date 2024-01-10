9085a9f521f09edbe66df09e49cdfa5f10b19a63
luhy01 <idukelu@qq.com>
2024-01-10 17:40:17 +0800

    Fucking Question 96

---
+++ leetcode/96.不同的二叉搜索树.go
@@ -0,0 +1,71 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=96 lang=golang
+ *
+ * [96] 不同的二叉搜索树
+ *
+ * https://leetcode.cn/problems/unique-binary-search-trees/description/
+ *
+ * algorithms
+ * Medium (70.93%)
+ * Likes:    2433
+ * Dislikes: 0
+ * Total Accepted:    412.7K
+ * Total Submissions: 581.7K
+ * Testcase Example:  '3'
+ *
+ * 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 3
+ * 输出：5
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 1
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+func numTrees(n int) int {
+
+	dp := make([]int, n+1)
+	dp[0] = 1
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+
+	return dp[n]
+}
+
+// @lc code=end
+
+// dp[3]，就是 元素1为头结点搜索树的数量 + 元素2为头结点搜索树的数量 + 元素3为头结点搜索树的数量
+// 元素1为头结点搜索树的数量 = 右子树有2个元素的搜索树数量 * 左子树有0个元素的搜索树数量
+// 元素2为头结点搜索树的数量 = 右子树有1个元素的搜索树数量 * 左子树有1个元素的搜索树数量
+// 元素3为头结点搜索树的数量 = 右子树有0个元素的搜索树数量 * 左子树有2个元素的搜索树数量
+// 有2个元素的搜索树数量就是dp[2]。
+// 有1个元素的搜索树数量就是dp[1]。
+// 有0个元素的搜索树数量就是dp[0]。
+// dp[3] = dp[1] + dp[2] + dp[3]
+// dp[3] = dp[2]*dp[0] + dp[1]*dp[1] + dp[0]*dp[2]
